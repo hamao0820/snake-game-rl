0bca3894c030e6387e91301b90aa89640c7b70c3
浜田　俊祐 <hamashun0820@icloud.com>
2023-09-20 09:39:10 +0900

    npmをbunに置き換えた

--- typescript/src/simulater/simulator.ts
+++ typescript/src/simulater/simulator.ts
@@ -1,3 +1,6 @@
+/// <reference lib="dom" />
+/// <reference lib="dom.iterable" />
+
 import puppeteer, { Browser, ElementHandle, Page } from 'puppeteer';
 import path from 'path';
 import Model from '../snake-game/ts/model';
