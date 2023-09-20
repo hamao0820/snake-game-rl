502572957a037c42319a0d8f17965c945750bdd6
浜田　俊祐 <hamashun0820@icloud.com>
2023-09-20 12:49:57 +0900

    画像を保存できるようにした

--- python/src/server/server.py
+++ python/src/server/server.py
@@ -1,15 +1,20 @@
 from websockets.sync.client import connect
 from time import sleep
 import json
+import PIL.Image as Image
+import io
 
 url = "ws://localhost:8080"
 
 with connect(url) as ws:
     ws.recv()
+    i = 0
     while True:
         sleep(0.3)
         ws.send("0")
         data = json.loads(ws.recv())
-        print(data["done"])
-        if (data["done"]):
+        img = Image.open(io.BytesIO(bytes(data["img"]["data"])))
+        img.save(f"./img/{i}.png")
+        i += 1
+        if data["done"]:
             break
