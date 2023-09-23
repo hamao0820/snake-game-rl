b5c70a68835987406d1bfc46389812a81a799ed0
浜田　俊祐 <hamashun0820@icloud.com>
2023-09-24 00:30:02 +0900

    envのresetでnp.arrayをそのまま返すようにした PIL.Imageをcv2に置き換えた

--- python/src/model/environment.py
+++ python/src/model/environment.py
@@ -4,6 +4,7 @@ from typing import List, Literal, Tuple, TypeAlias
 
 import numpy as np
 import PIL.Image as Image
+import cv2
 
 sys.path.append("./src/websocketClient")
 from websocket_client import Action, WebsocketClient
@@ -21,10 +22,10 @@ class SnakeGameEnv:
 
     def reset(self) -> Tuple[np.ndarray, dict]:
         state, info = self.__client.reset()
-        frame_image = Image.open(io.BytesIO(bytes(state["data"]))).convert("RGB")
-        self.__frames = [np.array(frame_image)]
-        state = np.transpose(np.expand_dims(np.array(frame_image.resize((84, 84)).convert("L")), axis=2), (2, 0, 1))
-        return state, info
+        frame_image = cv2.imdecode(np.frombuffer(io.BytesIO(bytes(state["data"])).read(), np.uint8), cv2.IMREAD_COLOR)
+        frame_array = np.array(frame_image)
+        self.__frames = [frame_array]
+        return frame_array, info
 
     def step(self, action: Action) -> Tuple[np.ndarray, float, bool, bool, dict]:
         observation, reward, terminated, truncated, info = self.__client.step(action)
