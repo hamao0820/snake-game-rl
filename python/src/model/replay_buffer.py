36aeef0ca2a8baa61e2dfd069f29dbfb6e886b04
浜田　俊祐 <hamashun0820@icloud.com>
2023-09-24 09:25:47 +0900

    replay_bufferを変更

--- python/src/model/replay_buffer.py
+++ python/src/model/replay_buffer.py
@@ -1,30 +1,19 @@
 import random
-from collections import deque
-from typing import Tuple
+from collections import deque, namedtuple
+from typing import List
 
-import numpy as np
+Transition = namedtuple("Transition", ("state", "action", "next_state", "reward"))
 
 
-class ReplayBuffer:
-    def __init__(self, buffer_size: int, batch_size: int) -> None:
-        self.buffer: deque[Tuple[np.ndarray, int, float, np.ndarray, bool]] = deque(maxlen=buffer_size)
+class ReplayMemory(object):
+    def __init__(self, capacity: int):
+        self.memory: deque[Transition] = deque(maxlen=capacity)
 
-        self.batch_size = batch_size
+    def push(self, *args) -> None:
+        self.memory.append(Transition(*args))
 
-    def add(self, state: np.ndarray, action: int, reward: float, next_state, done: bool) -> None:
-        data = (state, action, reward, next_state, done)
-
-        self.buffer.append(data)
+    def sample(self, batch_size) -> List[Transition]:  # batch_size分ランダムにメモリから抽出
+        return random.sample(self.memory, batch_size)
 
     def __len__(self) -> int:
-        return len(self.buffer)
-
-    def get_batch(self) -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
-        data = random.sample(self.buffer, self.batch_size)
-
-        state = np.stack([x[0] for x in data], axis=0, dtype=np.float32)
-        action = np.array([x[1] for x in data], dtype=np.int32)
-        reward = np.array([x[2] for x in data], dtype=np.float32)
-        next_state = np.stack([x[3] for x in data], axis=0, dtype=np.float32)
-        done = np.array([x[4] for x in data], dtype=np.int32)
-        return state, action, reward, next_state, done
+        return len(self.memory)
