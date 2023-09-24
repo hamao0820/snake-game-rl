4445be4283329de7e105dc30331397bf27052d02
浜田　俊祐 <hamashun0820@icloud.com>
2023-09-24 09:27:26 +0900

    nnを作り直した

--- python/src/model/QNet.py
+++ python/src/model/QNet.py
@@ -1,65 +1,36 @@
+import torch
 import torch.nn as nn
-from UNet import UNet
 
 
-class QNet(nn.Module):
-    def __init__(self, normalize_image=True) -> None:
-        super(QNet, self).__init__()
-
-        number_of_outputs = 3
-        self.normalize_image = normalize_image
-
-        self.conv1 = nn.Conv2d(in_channels=1, out_channels=64, kernel_size=3, stride=1)
-        self.conv2 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1)
-        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.conv3 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=1)
-        self.conv4 = nn.Conv2d(in_channels=128, out_channels=512, kernel_size=3, stride=1)
-        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.conv5 = nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1)
-        self.conv6 = nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1)
-        self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2)
-
-        self.advantage1 = nn.Linear(25088, 128)
-        self.advantage2 = nn.Linear(128, number_of_outputs)
-
-        self.value1 = nn.Linear(25088, 128)
-        self.value2 = nn.Linear(128, 1)
-
+class Dueling_Network(nn.Module):
+    def __init__(self, n_frame, n_actions) -> None:
+        super(Dueling_Network, self).__init__()
+        self.relu = nn.ReLU()
+        self.conv1 = nn.Conv2d(n_frame, 32, 8, 4)
+        self.conv2 = nn.Conv2d(32, 64, 4, 2)
+        self.conv3 = nn.Conv2d(64, 64, 3, 1)
+        self.act_fc = nn.Linear(3136, 512)
+        self.act_fc2 = nn.Linear(512, n_actions)
+        self.value_fc = nn.Linear(3136, 512)
+        self.value_fc2 = nn.Linear(512, 1)
+        torch.nn.init.kaiming_normal_(self.conv1.weight)
+        torch.nn.init.kaiming_normal_(self.conv2.weight)
+        torch.nn.init.kaiming_normal_(self.conv3.weight)
+        torch.nn.init.kaiming_normal_(self.act_fc.weight)
+        torch.nn.init.kaiming_normal_(self.act_fc2.weight)
+        torch.nn.init.kaiming_normal_(self.value_fc.weight)
+        torch.nn.init.kaiming_normal_(self.value_fc2.weight)
         self.flatten = nn.Flatten()
 
-        # self.activation = nn.Tanh()
-        self.activation = nn.ReLU()
-
     def forward(self, x):
-        if self.normalize_image:
-            x = x / 255
-
-        output_conv = self.conv1(x)
-        output_conv = self.activation(output_conv)
-        output_conv = self.conv2(output_conv)
-        output_conv = self.activation(output_conv)
-        output_conv = self.pool1(output_conv)
-        output_conv = self.conv3(output_conv)
-        output_conv = self.activation(output_conv)
-        output_conv = self.conv4(output_conv)
-        output_conv = self.activation(output_conv)
-        output_conv = self.pool2(output_conv)
-        output_conv = self.conv5(output_conv)
-        output_conv = self.activation(output_conv)
-        output_conv = self.conv6(output_conv)
-        output_conv = self.activation(output_conv)
-        output_conv = self.pool3(output_conv)
-
-        output_conv = self.flatten(output_conv)
-
-        output_advantage = self.advantage1(output_conv)
-        output_advantage = self.activation(output_advantage)
-        output_advantage = self.advantage2(output_advantage)
-
-        output_value = self.value1(output_conv)
-        output_value = self.activation(output_value)
-        output_value = self.value2(output_value)
-
-        output_final = output_value + output_advantage - output_advantage.mean()
-
-        return output_final
+        x = self.relu(self.conv1(x))
+        x = self.relu(self.conv2(x))
+        x = self.relu(self.conv3(x))
+        x = self.flatten(x)
+        x_act = self.relu(self.act_fc(x))
+        x_act = self.act_fc2(x_act)
+        x_val = self.relu(self.value_fc(x))
+        x_val = self.value_fc2(x_val)
+        x_act_ave = torch.mean(x_act, dim=1, keepdim=True)
+        q = x_val + x_act - x_act_ave
+        return q
