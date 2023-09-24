ce84d476ebed14a763387ef0f2b26315802b466d
浜田　俊祐 <hamashun0820@icloud.com>
2023-09-24 09:26:10 +0900

    agentを作り直した

--- python/src/model/agent.py
+++ python/src/model/agent.py
@@ -1,74 +1,82 @@
-import copy
+import math
+import random
+from typing import Tuple
 
-import numpy as np
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 import torch.optim as optim
-from QNet import QNet
-from replay_buffer import ReplayBuffer
+from QNet import Dueling_Network
+from replay_buffer import ReplayMemory, Transition
 
 
 class DQNAgent:
-    def __init__(self, device: torch.device):
-        self.gamma = 0.98
-        self.lr = 0.0005
-        self.epsilon = 0.3
-        self.buffer_size = 300
-        self.batch_size = 32
-        self.action_size = 3
+    GAMMA = 0.99
+    EPS_START = 0.9
+    EPS_END = 0.05
+    EPS_DECAY = 200
+    N_ACTIONS = 3
+    SIZE_REPLAY_MEMORY = 1000
+    BATCH_SIZE = 32
+    TAU = 0.005
+    LR = 1e-4
 
+    def __init__(self, n_frame: int, device: torch.device):
+        self.policy_net = Dueling_Network(n_frame=n_frame, n_actions=self.N_ACTIONS).to(device)
+        self.target_net = Dueling_Network(n_frame=n_frame, n_actions=self.N_ACTIONS).to(device)
         self.device = device
+        self.memory = ReplayMemory(self.SIZE_REPLAY_MEMORY)
+        self.optimizer = optim.AdamW(self.policy_net.parameters(), lr=self.LR, amsgrad=True)
 
-        self.replay_buffer = ReplayBuffer(self.buffer_size, self.batch_size)
-        self.qnet = QNet(self.action_size).to(self.device)
-        self.qnet.to(self.device)
-        self.qnet_target = QNet(self.action_size)
-        self.qnet_target.to(self.device)
-        self.optimizer = optim.Adam(self.qnet.parameters(), lr=self.lr)
+    def e_greedy_select_action(self, state: torch.Tensor, steps_done) -> Tuple[torch.Tensor, float]:
+        eps_threshold = self.EPS_END + (self.EPS_START - self.EPS_END) * math.exp(-1.0 * steps_done / self.EPS_DECAY)
+        steps_done += 1
 
-    def sync_qnet(self) -> None:
-        self.qnet_target = copy.deepcopy(self.qnet).to(self.device)
+        if random.random() > eps_threshold:
+            with torch.no_grad():
+                return self.policy_net(state.to(self.device)).argmax().view(1, 1).cpu(), eps_threshold
+        return torch.tensor([[random.randint(0, self.N_ACTIONS - 1)]]), eps_threshold
 
-    def get_action(self, state: np.ndarray) -> int:
-        if np.random.rand() < self.epsilon:
-            return np.random.choice(self.action_size)
-        else:
-            state = state[np.newaxis, :]
-
-            qs = self.qnet(torch.from_numpy(state).to(self.device))
-
-            return qs.data.argmax().item()
-
-    def update(self, state: np.ndarray, action: int, reward: float, next_state: np.ndarray, done: bool) -> float:
-        self.replay_buffer.add(state, action, reward, next_state, done)
-
-        if len(self.replay_buffer) < self.batch_size:
-            return 0
-
-        state_batch, action_batch, reward_batch, next_state_batch, done_batch = self.replay_buffer.get_batch()
-
-        qs = self.qnet(torch.from_numpy(state_batch).to(self.device))
-        q = qs[np.arange(self.batch_size), action_batch]
-
-        next_qs = self.qnet_target(torch.from_numpy(next_state_batch).to(self.device))
-        next_q = torch.max(next_qs, dim=1, keepdim=True).values.detach().squeeze().to(self.device)
-
-        target = (
-            torch.tensor(reward_batch).to(self.device)
-            + (1 - torch.tensor(done_batch).to(self.device)) * self.gamma * next_q
+    def update(self) -> None:
+        if len(self.memory) < self.BATCH_SIZE:
+            return
+        transitions = self.memory.sample(self.BATCH_SIZE)
+        batch = Transition(*zip(*transitions))
+        non_final_mask = torch.tensor(
+            tuple(map(lambda s: s is not None, batch.next_state)), device=self.device, dtype=torch.bool
         )
+        non_final_next_states = torch.cat([s for s in batch.next_state if s is not None])
 
-        loss = F.mse_loss(q, target).to(self.device)
+        state_batch = torch.cat(batch.state).to(self.device)
+        action_batch = torch.cat(batch.action).to(self.device)
+        reward_batch = torch.cat(batch.reward).to(self.device)
 
-        self.qnet.zero_grad()
+        state_action_values = self.policy_net(state_batch).gather(1, action_batch)
+        next_state_values = torch.zeros(self.BATCH_SIZE, device=self.device)
 
+        with torch.no_grad():
+            next_state_values[non_final_mask] = self.target_net(non_final_next_states.to(self.device)).max(1)[0]
+
+        expected_state_action_values = (next_state_values * self.GAMMA) + reward_batch
+
+        criterion = nn.SmoothL1Loss()
+        loss = criterion(state_action_values, expected_state_action_values.unsqueeze(1))
+        self.optimizer.zero_grad()
         loss.backward()
-
+        torch.nn.utils.clip_grad_value_(self.policy_net.parameters(), 1000)
         self.optimizer.step()
 
-        return loss.data.item()
+    def push_memory(self, *args) -> None:
+        self.memory.push(*args)
 
-    def save(self, name: str = "dqn_weight") -> None:
-        torch.save(self.qnet.state_dict(), "model_weights/" + name + ".pth")
-        torch.save(self.qnet_target.state_dict(), "model_weights/" + name + "_target" + ".pth")
+    def sync_target(self) -> None:
+        target_net_state_dict = self.target_net.state_dict()
+        policy_net_state_dict = self.policy_net.state_dict()
+        for key in policy_net_state_dict:
+            target_net_state_dict[key] = policy_net_state_dict[key] * self.TAU + target_net_state_dict[key] * (
+                1 - self.TAU
+            )
+        self.target_net.load_state_dict(target_net_state_dict)
+
+    def save(self, fname: str) -> None:
+        torch.save(self.target_net.state_dict(), f"model_weights/{fname}_target.pth")
+        torch.save(self.policy_net.state_dict(), f"model_weights/{fname}_policy.pth")
