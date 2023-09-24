280b1ab30168056d679e55914ba53d16474e5d3f
浜田　俊祐 <hamashun0820@icloud.com>
2023-09-24 09:27:00 +0900

    envを作り直した

--- python/src/model/environment.py
+++ python/src/model/environment.py
@@ -2,9 +2,9 @@ import io
 import sys
 from typing import List, Literal, Tuple, TypeAlias
 
+import cv2
 import numpy as np
 import PIL.Image as Image
-import cv2
 
 sys.path.append("./src/websocketClient")
 from websocket_client import Action, WebsocketClient
@@ -12,10 +12,24 @@ from websocket_client import Action, WebsocketClient
 Action_Space: TypeAlias = Tuple[Literal[0], Literal[1], Literal[2]]
 
 
+class ActionSpace:
+    def __init__(self) -> None:
+        self.__action_space: Action_Space = (0, 1, 2)
+        return
+
+    @property
+    def action_space(self) -> Action_Space:
+        return self.__action_space
+
+    @property
+    def n(self) -> int:
+        return len(self.__action_space)
+
+
 class SnakeGameEnv:
     def __init__(self) -> None:
         self.__client = WebsocketClient()
-        self.__action_space: Action_Space = (0, 1, 2)
+        self.__action_space = ActionSpace()
         self.__client.connect()
         self.__frames: List[np.ndarray] = []
         return
@@ -29,18 +43,20 @@ class SnakeGameEnv:
 
     def step(self, action: Action) -> Tuple[np.ndarray, float, bool, bool, dict]:
         observation, reward, terminated, truncated, info = self.__client.step(action)
-        frame_image = Image.open(io.BytesIO(bytes(observation["data"]))).convert("RGB")
-        self.__frames.append(np.array(frame_image))
-        state = np.transpose(np.expand_dims(np.array(frame_image.resize((84, 84)).convert("L")), axis=2), (2, 0, 1))
-        return state, reward, terminated, truncated, info
+        frame_image = cv2.imdecode(
+            np.frombuffer(io.BytesIO(bytes(observation["data"])).read(), np.uint8), cv2.IMREAD_COLOR
+        )
+        frame_array = np.array(frame_image)
+        self.__frames.append(frame_array)
+        return frame_array, reward, terminated, truncated, info
 
     def close(self) -> None:
         self.__client.close()
 
-    def render(self, path: str = "img/snake-game.gif") -> None:
+    def render(self, fname: str = "snake-game") -> None:
         images = [Image.fromarray(frame) for frame in self.__frames]
         images[0].save(
-            path,
+            f"img/{fname}.gif",
             save_all=True,
             append_images=images[1:],
             duration=100,
@@ -48,5 +64,5 @@ class SnakeGameEnv:
         )
 
     @property
-    def action_space(self) -> Action_Space:
+    def action_space(self) -> ActionSpace:
         return self.__action_space
