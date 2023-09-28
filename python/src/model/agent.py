d06379ed314b46efc2e49ba341722a3d1c881dc4
浜田　俊祐 <hamashun0820@icloud.com>
2023-09-28 20:40:47 +0900

    static変数の呼び出し方を変更

--- python/src/model/agent.py
+++ python/src/model/agent.py
@@ -21,28 +21,28 @@ class DQNAgent:
     LR = 1e-4
 
     def __init__(self, n_frame: int, device: torch.device, state_dict_path: Union[str, None] = None):
-        self.policy_net = Dueling_Network(n_frame=n_frame, n_actions=self.N_ACTIONS).to(device)
-        self.target_net = Dueling_Network(n_frame=n_frame, n_actions=self.N_ACTIONS).to(device)
+        self.policy_net = Dueling_Network(n_frame=n_frame, n_actions=DQNAgent.N_ACTIONS).to(device)
+        self.target_net = Dueling_Network(n_frame=n_frame, n_actions=DQNAgent.N_ACTIONS).to(device)
         if state_dict_path is not None:
             self.policy_net.load_state_dict(torch.load(state_dict_path))
         self.target_net.load_state_dict(self.policy_net.state_dict())
         self.device = device
-        self.memory = ReplayMemory(self.SIZE_REPLAY_MEMORY)
-        self.optimizer = optim.AdamW(self.policy_net.parameters(), lr=self.LR, amsgrad=True)
+        self.memory = ReplayMemory(DQNAgent.SIZE_REPLAY_MEMORY)
+        self.optimizer = optim.AdamW(self.policy_net.parameters(), lr=DQNAgent.LR, amsgrad=True)
 
     def e_greedy_select_action(self, state: torch.Tensor, steps_done) -> Tuple[torch.Tensor, float]:
-        eps_threshold = self.EPS_END + (self.EPS_START - self.EPS_END) * math.exp(-1.0 * steps_done / self.EPS_DECAY)
+        eps_threshold = DQNAgent.EPS_END + (DQNAgent.EPS_START - DQNAgent.EPS_END) * math.exp(-1.0 * steps_done / DQNAgent.EPS_DECAY)
         steps_done += 1
 
         if random.random() > eps_threshold:
             with torch.no_grad():
                 return self.policy_net(state.to(self.device)).argmax().view(1, 1).cpu(), eps_threshold
-        return torch.tensor([[random.randint(0, self.N_ACTIONS - 1)]]), eps_threshold
+        return torch.tensor([[random.randint(0, DQNAgent.N_ACTIONS - 1)]]), eps_threshold
 
     def update(self) -> None:
-        if len(self.memory) < self.BATCH_SIZE:
+        if len(self.memory) < DQNAgent.BATCH_SIZE:
             return
-        transitions = self.memory.sample(self.BATCH_SIZE)
+        transitions = self.memory.sample(DQNAgent.BATCH_SIZE)
         batch = Transition(*zip(*transitions))
         non_final_mask = torch.tensor(
             tuple(map(lambda s: s is not None, batch.next_state)), device=self.device, dtype=torch.bool
@@ -54,12 +54,12 @@ class DQNAgent:
         reward_batch = torch.cat(batch.reward).to(self.device)
 
         state_action_values = self.policy_net(state_batch).gather(1, action_batch)
-        next_state_values = torch.zeros(self.BATCH_SIZE, device=self.device)
+        next_state_values = torch.zeros(DQNAgent.BATCH_SIZE, device=self.device)
 
         with torch.no_grad():
             next_state_values[non_final_mask] = self.target_net(non_final_next_states.to(self.device)).max(1)[0]
 
-        expected_state_action_values = (next_state_values * self.GAMMA) + reward_batch
+        expected_state_action_values = (next_state_values * DQNAgent.GAMMA) + reward_batch
 
         criterion = nn.SmoothL1Loss()
         loss = criterion(state_action_values, expected_state_action_values.unsqueeze(1))
@@ -75,8 +75,8 @@ class DQNAgent:
         target_net_state_dict = self.target_net.state_dict()
         policy_net_state_dict = self.policy_net.state_dict()
         for key in policy_net_state_dict:
-            target_net_state_dict[key] = policy_net_state_dict[key] * self.TAU + target_net_state_dict[key] * (
-                1 - self.TAU
+            target_net_state_dict[key] = policy_net_state_dict[key] * DQNAgent.TAU + target_net_state_dict[key] * (
+                1 - DQNAgent.TAU
             )
         self.target_net.load_state_dict(target_net_state_dict)
 
