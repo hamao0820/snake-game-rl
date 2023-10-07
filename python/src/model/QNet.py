d36fa96f2db9f4d9bef38e245d3bfd0ff21c43af
浜田　俊祐 <hamashun0820@icloud.com>
2023-10-07 10:58:32 +0900

    coordConvを実装

--- python/src/model/QNet.py
+++ python/src/model/QNet.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.modules.conv as conv
 
 
 class Dueling_Network(nn.Module):
@@ -7,20 +8,17 @@ class Dueling_Network(nn.Module):
         super(Dueling_Network, self).__init__()
         self.relu = nn.ReLU()
         self.sigmoid = nn.Sigmoid()
-        self.conv1 = nn.Conv2d(n_frame, 32, 3, 1)
+
+        self.coord_conv = CoordConv2d(n_frame, 32, 3, 1)
         self.pool1 = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(32, 64, 3, 1)
-        self.attention_conv1 = nn.Conv2d(64, 64, 3, 1, 1)
-        self.attention_conv2 = nn.Conv2d(64, 64, 3, 1, 1)
-        self.conv3 = nn.Conv2d(64, 64, 3, 1)
-        self.pool2 = nn.MaxPool2d(2, 2)
-        self.act_fc = nn.Linear(20736, 512)
+        self.conv = nn.Conv2d(32, 64, 3, 1)
+
+        self.act_fc = nn.Linear(97344, 512)
         self.act_fc2 = nn.Linear(512, n_actions)
-        self.value_fc = nn.Linear(20736, 512)
+        self.value_fc = nn.Linear(97344, 512)
         self.value_fc2 = nn.Linear(512, 1)
-        torch.nn.init.kaiming_normal_(self.conv1.weight)
-        torch.nn.init.kaiming_normal_(self.conv2.weight)
-        torch.nn.init.kaiming_normal_(self.conv3.weight)
+        torch.nn.init.kaiming_normal_(self.coord_conv.weight)
+        torch.nn.init.kaiming_normal_(self.conv.weight)
         torch.nn.init.kaiming_normal_(self.act_fc.weight)
         torch.nn.init.kaiming_normal_(self.act_fc2.weight)
         torch.nn.init.kaiming_normal_(self.value_fc.weight)
@@ -28,15 +26,11 @@ class Dueling_Network(nn.Module):
         self.flatten = nn.Flatten()
 
     def forward(self, x):
-        x = self.relu(self.conv1(x))
+        x = self.relu(self.coord_conv(x))
         x = self.pool1(x)
-        x = self.relu(self.conv2(x))
-        attention = self.relu(self.attention_conv1(x))
-        attention = self.sigmoid(self.attention_conv2(attention))
-        x = x * attention
-        x = self.relu(self.conv3(x))
-        x = self.pool2(x)
+        x = self.relu(self.conv(x))
         x = self.flatten(x)
+
         x_act = self.relu(self.act_fc(x))
         x_act = self.act_fc2(x_act)
         x_val = self.relu(self.value_fc(x))
@@ -44,3 +38,152 @@ class Dueling_Network(nn.Module):
         x_act_ave = torch.mean(x_act, dim=1, keepdim=True)
         q = x_val + x_act - x_act_ave
         return q
+
+
+class CoordConv2d(conv.Conv2d):
+    def __init__(
+        self,
+        in_channels,
+        out_channels,
+        kernel_size,
+        stride=1,
+        padding=0,
+        dilation=1,
+        groups=1,
+        bias=True,
+        with_r=False,
+        use_mps=True,
+    ):
+        super(CoordConv2d, self).__init__(
+            in_channels, out_channels, kernel_size, stride, padding, dilation, groups, bias
+        )
+        self.rank = 2
+        self.addcoords = AddCoords(self.rank, with_r, use_mps=use_mps)
+        self.conv = nn.Conv2d(
+            in_channels + self.rank + int(with_r), out_channels, kernel_size, stride, padding, dilation, groups, bias
+        )
+
+    def forward(self, input_tensor):
+        """
+        input_tensor_shape: (N, C_in,H,W)
+        output_tensor_shape: N,C_out,H_out,W_out）
+        :return: CoordConv2d Result
+        """
+        out = self.addcoords(input_tensor)
+        out = self.conv(out)
+
+        return out
+
+
+class AddCoords(nn.Module):
+    def __init__(self, rank, with_r=False, use_mps=True):
+        super(AddCoords, self).__init__()
+        self.rank = rank
+        self.with_r = with_r
+        self.use_mps = use_mps
+
+    def forward(self, input_tensor):
+        """
+        :param input_tensor: shape (N, C_in, H, W)
+        :return:
+        """
+        if self.rank == 1:
+            batch_size_shape, channel_in_shape, dim_x = input_tensor.shape
+            xx_range = torch.arange(dim_x, dtype=torch.int32)
+            xx_channel = xx_range[None, None, :]
+
+            xx_channel = xx_channel.float() / (dim_x - 1)
+            xx_channel = xx_channel * 2 - 1
+            xx_channel = xx_channel.repeat(batch_size_shape, 1, 1)
+
+            if torch.backends.mps.is_available() and self.use_mps:
+                input_tensor = input_tensor.cuda()
+                xx_channel = xx_channel.cuda()
+            out = torch.cat([input_tensor, xx_channel], dim=1)
+
+            if self.with_r:
+                rr = torch.sqrt(torch.pow(xx_channel - 0.5, 2))
+                out = torch.cat([out, rr], dim=1)
+
+        elif self.rank == 2:
+            batch_size_shape, channel_in_shape, dim_y, dim_x = input_tensor.shape
+            xx_ones = torch.ones([1, 1, 1, dim_x], dtype=torch.int32)
+            yy_ones = torch.ones([1, 1, 1, dim_y], dtype=torch.int32)
+
+            xx_range = torch.arange(dim_y, dtype=torch.int32)
+            yy_range = torch.arange(dim_x, dtype=torch.int32)
+            xx_range = xx_range[None, None, :, None]
+            yy_range = yy_range[None, None, :, None]
+
+            xx_channel = torch.matmul(xx_range, xx_ones)
+            yy_channel = torch.matmul(yy_range, yy_ones)
+
+            # transpose y
+            yy_channel = yy_channel.permute(0, 1, 3, 2)
+
+            xx_channel = xx_channel.float() / (dim_y - 1)
+            yy_channel = yy_channel.float() / (dim_x - 1)
+
+            xx_channel = xx_channel * 2 - 1
+            yy_channel = yy_channel * 2 - 1
+
+            xx_channel = xx_channel.repeat(batch_size_shape, 1, 1, 1)
+            yy_channel = yy_channel.repeat(batch_size_shape, 1, 1, 1)
+
+            if torch.backends.mps.is_available() and self.use_mps:
+                device = torch.device("mps")
+                input_tensor = input_tensor.to(device)
+                xx_channel = xx_channel.to(device)
+                yy_channel = yy_channel.to(device)
+            out = torch.cat([input_tensor, xx_channel, yy_channel], dim=1)
+
+            if self.with_r:
+                rr = torch.sqrt(torch.pow(xx_channel - 0.5, 2) + torch.pow(yy_channel - 0.5, 2))
+                out = torch.cat([out, rr], dim=1)
+
+        elif self.rank == 3:
+            batch_size_shape, channel_in_shape, dim_z, dim_y, dim_x = input_tensor.shape
+            xx_ones = torch.ones([1, 1, 1, 1, dim_x], dtype=torch.int32)
+            yy_ones = torch.ones([1, 1, 1, 1, dim_y], dtype=torch.int32)
+            zz_ones = torch.ones([1, 1, 1, 1, dim_z], dtype=torch.int32)
+
+            xy_range = torch.arange(dim_y, dtype=torch.int32)
+            xy_range = xy_range[None, None, None, :, None]
+
+            yz_range = torch.arange(dim_z, dtype=torch.int32)
+            yz_range = yz_range[None, None, None, :, None]
+
+            zx_range = torch.arange(dim_x, dtype=torch.int32)
+            zx_range = zx_range[None, None, None, :, None]
+
+            xy_channel = torch.matmul(xy_range, xx_ones)
+            xx_channel = torch.cat([xy_channel + i for i in range(dim_z)], dim=2)
+            xx_channel = xx_channel.repeat(batch_size_shape, 1, 1, 1, 1)
+
+            yz_channel = torch.matmul(yz_range, yy_ones)
+            yz_channel = yz_channel.permute(0, 1, 3, 4, 2)
+            yy_channel = torch.cat([yz_channel + i for i in range(dim_x)], dim=4)
+            yy_channel = yy_channel.repeat(batch_size_shape, 1, 1, 1, 1)
+
+            zx_channel = torch.matmul(zx_range, zz_ones)
+            zx_channel = zx_channel.permute(0, 1, 4, 2, 3)
+            zz_channel = torch.cat([zx_channel + i for i in range(dim_y)], dim=3)
+            zz_channel = zz_channel.repeat(batch_size_shape, 1, 1, 1, 1)
+
+            if torch.backends.mps.is_available() and self.use_mps:
+                device = torch.device("mps")
+                input_tensor = input_tensor.to(device)
+                xx_channel = xx_channel.to(device)
+                yy_channel = yy_channel.to(device)
+                zz_channel = zz_channel.to(device)
+            out = torch.cat([input_tensor, xx_channel, yy_channel, zz_channel], dim=1)
+
+            if self.with_r:
+                rr = torch.sqrt(
+                    torch.pow(xx_channel - 0.5, 2) + torch.pow(yy_channel - 0.5, 2) + torch.pow(zz_channel - 0.5, 2)
+                )
+                out = torch.cat([out, rr], dim=1)
+        else:
+            raise NotImplementedError
+
+        return out
